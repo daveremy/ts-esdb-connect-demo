787657e878acfa144b9110bfaefab46499430c84
Dave Remy <email@daveremy.com>
2024-04-04 06:53:52 -0700

    some simple refactoring of server side post for esdb messages

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,9 +1,14 @@
-import express from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import path from 'path';
-import { LoanApplication } from './LoanApplication';
-import { StateChangeEvent, LoanEvent, EventType, EventData } from "../shared/types";
+import express from "express";
+import { createServer, IncomingHttpHeaders } from "http";
+import { Server } from "socket.io";
+import path from "path";
+import { LoanApplication } from "./LoanApplication";
+import {
+  StateChangeEvent,
+  LoanEvent,
+  EventType,
+  EventData,
+} from "../shared/types";
 
 const app = express();
 const httpServer = createServer(app);
@@ -12,69 +17,89 @@ const io = new Server(httpServer, {
   cors: {
     origin: "http://localhost:8080", // Assuming your client runs on this port with webpack-dev-server
     methods: ["GET", "POST"],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 const PORT = 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../../dist/client')));
+app.use(express.static(path.join(__dirname, "../../dist/client")));
 
 // Map to keep active loan applications
 const activeLoanApps: { [loanId: string]: LoanApplication } = {};
 
 // Route to receive POST requests from the ESDB connector
-app.post('/event', (req, res) => {
-    const metadata = parseMetadata(req);
-    const event: LoanEvent = {
-        eventType: metadata['es-event-type'] as EventType,
-        data: req.body as EventData,
-    };
+app.post("/event", (request, response) => {
+  // Events from EventstoreDB are in the body (request.body)
+  //  Metadata is in the request.headers
+  const metadata = parseMetadata(request.headers);
+  const eventData = request.body;
 
-    const loanId = event.data.loanId; // Ensure your LoanEvent and EventData interfaces reflect the actual structure
+  // Bundle the Event Type and Event Data into LoanEvent for ease later
+  const event: LoanEvent = {
+    eventType: metadata["es-event-type"] as EventType,
+    data: eventData as EventData,
+  };
 
-    let loanApplication = activeLoanApps[loanId];
-    if (!loanApplication) {
-        loanApplication = new LoanApplication(loanId);
-        activeLoanApps[loanId] = loanApplication;
-    }
+  // Active loans applications are cached
+  let loanApplication = getLoanApplicationFromCacheOrNew(event);
 
-    const new_state = loanApplication.evolve(event);
-    const state_change: StateChangeEvent = {
-        new_state: new_state,
-        event: event,
-    };
+  // Evolve (apply the new event) the state of the loan application and get
+  //  new state
+  const new_state = loanApplication.evolve(event);
 
-    io.emit('state_change', state_change);
+  // Bundle the new state and the event that caused the new state together to
+  //  broadcast to interested web pages
+  const state_change: StateChangeEvent = {
+    new_state: new_state,
+    event: event,
+  };
 
-    if (loanApplication.isComplete()) {
-        delete activeLoanApps[loanId];
-    }
+  // Emit the state change to interested web pages
+  io.emit("state_change", state_change);
 
-    res.status(200).send('Event received');
+  response.status(200).send("Event received");
 });
 
-function parseMetadata(req: express.Request): { [key: string]: string | string[]; } {
-    return Object.keys(req.headers)
-        .filter((key) => key.startsWith('es-'))
-        .reduce((acc, key) => {
-            const value = req.headers[key];
-            if (value !== undefined) {
-                acc[key] = value;
-            }
-            return acc;
-        }, {} as { [key: string]: string | string[]; });
+function getLoanApplicationFromCacheOrNew(event: LoanEvent) {
+  const loanId = event.data.loanId;
+  let loanApplication = activeLoanApps[loanId];
+  if (!loanApplication) {
+    loanApplication = new LoanApplication(loanId);
+    activeLoanApps[loanId] = loanApplication;
+  }
+
+  if (loanApplication.isComplete()) {
+    delete activeLoanApps[loanId];
+  }
+
+  return loanApplication;
+}
+
+function parseMetadata(headers: IncomingHttpHeaders): {
+  [key: string]: string | string[];
+} {
+  // todo: change this to a map from an object
+  return Object.keys(headers)
+    .filter((key) => key.startsWith("es-"))
+    .reduce((acc, key) => {
+      const value = headers[key];
+      if (value !== undefined) {
+        acc[key] = value;
+      }
+      return acc;
+    }, {} as { [key: string]: string | string[] });
 }
 
 // Initialize Socket.IO connection
-io.on('connection', (socket) => {
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
+io.on("connection", (socket) => {
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
 
 // Start the server
 httpServer.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
